d8000fc0eb25b6ebf536978306e9b950cc86c4c5
gian luca pinna angioni <gian.luca.pinna.angioni@cern.ch>
2018-05-28 12:17:59 +0200

    explicit tree name request

--- Tools/lumi_utils.py
+++ Tools/lumi_utils.py
@@ -167,7 +167,7 @@ def makeBrilcalcMap():
 def getChunks(v,n=3): return [ v[i:i+n] for i in range(0, len(v), n) ]
 
 def getRunLumis(fnames, treename="Events"):
-    print "fnames", fnames
+    #print "fnames", fnames
     # returns dict where keys are runs and values are sets of lumi sections
     #print type(fnames)
     if type(fnames) == list:
@@ -177,7 +177,8 @@ def getRunLumis(fnames, treename="Events"):
 
     f1 = r.TFile(fname)
     treenames = [obj.GetName() for obj in f1.GetListOfKeys()]
-    treename = treenames[0]
+    treename = "DTTree" 
+
     if len(treenames) > 1 and "Events" in treenames:
         treename = "Events"
 
